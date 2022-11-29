62c7ea425f4dd16e9abb103b3bc827bde0a0a58f
Vladislav Rashkevich <88950535+VladislavRashkevich@users.noreply.github.com>
2022-11-29 12:12:21 +0300

    add some tests
    add some tests

--- test_file.py
+++ test_file.py
@@ -3,3 +3,11 @@ def test_one():
     
 def test_two():
     assert True
+    
+def test_three():
+    a = 2
+    assert a == 2
+    
+def test_fore():
+    a = 3
+    assert a == 2, "Num is not True" 
